74f58c510556bd9c3ff614ab02500034c4e6049c
pedro030 <pedrocontreras182@gmail.com>
2023-03-18 18:06:50 -0300

    layout productCard component

---
+++ src/components/product_card/productCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './productCard.scss'
+import productImage from '../../assets/salad.jpg'
+
+const ProductCard = () => {
+
+  return (
+    <div className='card'>
+        <img src={productImage} alt="product-image" className='card-image' width = "295" height = "240"/>
+        <div className="card-text">
+            <h3>Product Name</h3>
+            <p className="card-description">Product short description</p>
+            <h3>$50</h3>
+        </div>
+    </div>
+  )
+}
+
+  export default ProductCard
