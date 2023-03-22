cd155ece3a7c278a205307fbfa86d737ad999f29
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-03-22 11:42:24 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Component, useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Carousel from './components/carousel/Carousel'
+import ProductDetail from './components/product_detail/ProductDetail'
+import ProductCard from './components/product_card/productCard'
 
 
 
@@ -11,6 +13,9 @@ const  App = () => {
     <div>
         <Header />
         <Carousel />
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
     </div>
   )
 }
