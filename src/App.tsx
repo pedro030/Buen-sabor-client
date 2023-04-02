8a372d8a0539a5256edc3fab481069e404d3b555
pedro030 <pedrocontreras182@gmail.com>
2023-04-02 18:18:24 -0300

    migrate to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { Component, useState } from 'react'
-import './App.css'
+import React from 'react'
+import './App.scss'
 import Header from './components/header/Header'
 import Carousel from './components/carousel/Carousel'
-import ProductDetail from './components/product_detail/ProductDetail'
-import ProductCard from './components/product_card/productCard'
 import Menu from './components/menu/Menu'
 
 
--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Carousel.scss'
-import img_carousel from '../../assets/A0_MainDinning_Tight_v1s-1440x765.jpg'
-import clock from '../../assets/clock2.svg'
-import location from '../../assets/location.svg'
+import img_carousel from '../../assets/A0_MainDinning_Tight_v1s-1440x765.png'
+import img_location from '../../assets/location.svg'
+import img_clock from '../../assets/clock2.svg'
 
 const Carousel = () => {
     return (
@@ -19,14 +19,14 @@ const Carousel = () => {
                 <div className='info'>
 
                     <div className='detail_info'>
-                        <img src={location} height="50" />
+                        <img src={img_location} height="50" />
                         <div className="text">
                         <h2>Locate us</h2>
                         <p>Coronel Rodríguez 273, Mendoza</p>
                         </div>
                     </div>
                     <div className='detail_info'>
-                        <img src={clock} height="50" />
+                        <img src={img_clock} height="50" />
                         <div className="text">
                         <h2>Open hours</h2>
                         <p>Mon To Fri 9:00AM - 9:00PM</p>
