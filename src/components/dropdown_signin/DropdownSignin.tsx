4880933e95ea3fc6ff5bef892aaa71d2c7662e9f
pedro030 <pedrocontreras182@gmail.com>
2023-04-20 15:14:22 -0300

    implementing dropdown menu in nav for login

---
+++ src/components/dropdown_signin/DropdownSignin.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import './Dropdown.scss'
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
+
+const DropdownSignin = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        await loginWithRedirect();
+    };
+
+    const handleSignUp = async () => {
+        await loginWithRedirect({
+            authorizationParams: {
+                screen_hint: "signup",
+            },
+        });
+    };
+
+    const handleLogout = () => {
+        logout({
+            logoutParams: {
+                returnTo: window.location.origin,
+            },
+        });
+    };
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="dropdown" onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
+            <div className="dropdown__toggle">
+                <span className='button'>{!isAuthenticated ?"Sign In":"O"}</span>
+            </div>
+            {isOpen && (
+                <ul className="dropdown__list">
+                    {!isAuthenticated && (
+                        <>
+                        <li className='dropdown-item' onClick={handleLogin}>Log In</li>
+                        <li className='dropdown-item' onClick={handleSignUp}>Log Up</li>
+                        </>
+                    )}
+                    {isAuthenticated && (
+                        <>
+                            <li className='dropdown-item' onClick={() => navigate('myprofile')}>My Profile</li>
+                            <li className='dropdown-item' onClick={handleLogout}>Logout</li>
+                        </>
+                    )}
+                </ul>
+            )}
+        </div>
+    );
+}
+
+export default DropdownSignin
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,34 +3,11 @@ import './Header.scss'
 import setting from '../../assets/setting.svg'
 import bike from '../../assets/bike.svg'
 import cart from '../../assets/cart.svg'
-import { useAuth0 } from '@auth0/auth0-react'
-import { useNavigate } from 'react-router-dom'
+import DropdownSignin from '../dropdown_signin/DropdownSignin'
 // import searcher from '../../assets/searcher.svg'
 
 
 const  Header = () => {
-    const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
-    const navigate = useNavigate();
-
-    const handleLogin = async () => {
-        await loginWithRedirect();
-    };
-
-    const handleSignUp = async () => {
-        await loginWithRedirect({
-            authorizationParams: {
-                screen_hint: "signup",
-            },
-        });
-    };
-
-    const handleLogout = () => {
-        logout({
-            logoutParams: {
-                returnTo: window.location.origin,
-            },
-        });
-    };
 
     return (
         <nav className="nav">
@@ -39,37 +16,22 @@ const  Header = () => {
                     <h1>Buen Sabor</h1>
                     {/* <h4>by UTN</h4> */}
                 </div>
-                <div >
-                    <input className="searchInput" type='search'placeholder="         Search Food"></input>
-                    </div>
+                <div>
+                    <input className="searchInput" type='search' placeholder="Search Food"></input>
+                </div>
                     
-                    <ul className="items">
-                        <li><img src={setting} height="25"/></li>
+                <ul className="items">
+                    <li><img src={setting} height="25"/></li>
+                    {/* <li>
+                        <div className='circle'>
+                            <img src={bike} height="25"/>
+                            <div className='circle small_circle'>1</div>
+                        </div>                            
+                    </li> */}
 
-                        <li>
-                            <div className='circle'>
-                                <img src={bike} height="25"/>
-                                <div className='circle small_circle'>1</div>
-                            </div>
-                            
-                        </li>
-
-                        <li><img src={cart} height="25"/></li>
-                    {!isAuthenticated && (
-                        <>
-                            <li><button onClick={handleLogin}>Sign in</button></li>
-                            <li><button onClick={handleSignUp}>Sign Up</button></li>
-                        </>
-                    )}
-                    {isAuthenticated && (
-                        <>
-                        <li onClick={()=>navigate('myprofile')}>My Profile</li>
-                        <li><button onClick={handleLogout}>Logout</button></li>
-                        </>
-                    )}
-                    </ul>
-                    
-                
+                    <li><img src={cart} height="25"/></li>
+                    <DropdownSignin />
+                </ul>
             </div>
             <div className="under_nave">
                 <ul className="items">
