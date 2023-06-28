3e53da46e48f983e0eaaca68a8ac89380446d566
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-06-28 13:47:40 -0300

    update UserProfile page

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -6,23 +6,37 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 const UserProfile = () => {
   const { user } = useAuth0()
-  
+
   return (
-    <div className="user-profile-container">
-      <div className="sidebar">
-        <div className="">
-          <p>
-            My profile
-          </p>
-          <h3>{user?.given_name || "User"}</h3>
+
+    <div className='grid grid-cols-[260px_1fr]'>
+      <ul className="w-56 m-4 menu bg-secondary rounded-box">
+        <div className="flex justify-center avatar">
+          <div className="w-24 rounded-full">
+            <img src={user?.picture} />
+          </div>
         </div>
-        <ul>
-          <li>Account</li>
-        </ul>
-      </div>
-      <div className="">
+        <h3 className='text-center text-gray-600 menu-title'>{user?.given_name + ' ' + user?.family_name || "User"}</h3>
+        <li>
+          <h2 className="menu-title">Account</h2>
+          <ul>
+            <li><a>Info</a></li>
+            <li><a>Addresses</a></li>
+            <li><a>Change password</a></li>
+          </ul>
+        </li>
+        <li>
+          <h2 className="menu-title">Orders</h2>
+          <ul>
+            <li><a>Order list</a></li>
+            <li><a>Item 2</a></li>
+            <li><a>Item 3</a></li>
+          </ul>
+        </li>
+      </ul>
+      <div className='mt-4'>
         <Routes>
-          <Route path="/" element={<UserDetails/>} />
+          <Route path="/" element={<UserDetails />} />
         </Routes>
       </div>
     </div>
