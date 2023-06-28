b88fd069c5417ce114bd06a7845dcd78edcc648d
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-06-28 13:47:52 -0300

    update UserProfile Component

--- src/components/user_details/UserDetails.tsx
+++ src/components/user_details/UserDetails.tsx
@@ -5,28 +5,28 @@ import React from 'react'
 
 const UserDetails = () => {
 
-    const {user} = useAuth0()
+  const { user } = useAuth0()
   return (
-      <div className="user-detail-container">
-        <h2>Account Info</h2>
-          <Formik
-              initialValues={{
-                  firstName: user?.given_name,
-                  email: user?.email,
-                  lastName: user?.family_name,
-                  rememberMe: false
-              }}
-              onSubmit={(state) => { console.log(state.email) }}
-          >
-              <Form>
-                  <label htmlFor="firstName">First Name</label>
-                  <Field name="firstName" type="text" className="input-text" />
-                  <label htmlFor="lastName">Last Name</label>
-                  <Field type="text" name="lastName" className="input-text" />
-                  <label htmlFor="email">Email</label>
-                  <Field name="email" type="email" className="input-text" />
-              </Form>
-        </Formik>
+    <div className="">
+      <h2 className='stat-title'>Account Info</h2>
+      <Formik
+        initialValues={{
+          firstName: user?.given_name,
+          email: user?.email,
+          lastName: user?.family_name,
+          rememberMe: false
+        }}
+        onSubmit={(state) => { console.log(state.email) }}
+      >
+        <Form>
+          <label className='label' htmlFor="firstName"><span className="label-text">First Name</span></label>
+          <Field name="firstName" type="text" className="block w-full max-w-xs input input-bordered input-primary" />
+          <label className='label' htmlFor="lastName"><span className="label-text">Last Name</span></label>
+          <Field type="text" name="lastName" className="block w-full max-w-xs input input-bordered input-primary" />
+          <label className='label' htmlFor="email"><span className="label-text">Email</span></label>
+          <Field name="email" type="email" className="block w-full max-w-xs input input-bordered input-primary" />
+        </Form>
+      </Formik>
     </div>
   )
 }
