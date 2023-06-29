e4cd2586b224cb37c2c945819a6e5d04f06ef6ac
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-06-29 10:38:29 -0300

    update FE cliente

--- src/components/dropdown_signin/DropdownSignin.tsx
+++ src/components/dropdown_signin/DropdownSignin.tsx
@@ -34,10 +34,30 @@ const DropdownSignin = () => {
         setIsOpen(!isOpen);
     };
 
+    const swithButton = () => {
+        if (!isAuthenticated) {
+            return <button className='w-full rounded-full btn btn-primary'>Sign In</button>
+        } else {
+            return (
+                <div className='pt-2 hero'>
+                    <button>
+                        <div className="avatar">
+                            <div className="w-10 rounded-full">
+                                <img src={user?.picture} />
+                            </div>
+                        </div>
+                    </button>
+                </div>)
+        }
+    }
+
+    const { user } = useAuth0()
+
     return (
         <>
             <div className="w-full dropdown dropdown-end">
-                <button className='w-full rounded-full btn btn-primary'>{!isAuthenticated ? "Sign In" : "O"}</button>
+
+                {swithButton()}
 
                 <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
 
--- src/components/product_card/ProductCard.tsx
+++ src/components/product_card/ProductCard.tsx
@@ -5,22 +5,30 @@ const ProductCard = () => {
 
   return (
     <>
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
-      <figure><img src={productImage} alt="product-image" /></figure>
-      <div className="card-body">
-        <h2 className="card-title">Product Name</h2>
-        <p>Product short description</p>
-        <p>$50</p>
-        <div className="card-actions justify-center">
-          <button className="btn btn-primary btn-disabled">Add order</button>
+      <div className="shadow-xl card card-compact w-96 bg-base-100">
+        <div className="hero h-[14rem] rounded-md  bg-[url('src/assets/salad.jpg')]">
+          <div className="bg-opacity-80"></div>
+          <div className="hero-content ">
+            <div className="max-w-md">
+              
+            </div>
+          </div>
+        </div>
+        {/* <figure><img src={productImage} alt="product-image" /></figure> */}
+        <div className="card-body">
+          <h2 className="card-title">Product Name</h2>
+          <p>Product short description</p>
+          <p>$50</p>
+          <div className="justify-center card-actions">
+            <button className="btn btn-primary btn-disabled">Add order</button>
+          </div>
         </div>
       </div>
-    </div>
-    {/* onClick = {()=> window.my_modal_3.showModal()} ABRIR MODAL
+      {/* onClick = {()=> window.my_modal_3.showModal()} ABRIR MODAL
      <dialog id="my_modal_3" className="modal">
     <form method="dialog" className="modal-box">
-      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-      <h3 className="font-bold text-lg">Hello!</h3>
+      <button className="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">✕</button>
+      <h3 className="text-lg font-bold">Hello!</h3>
       <p className="py-4">Press ESC key or click on ✕ button to close</p>
     </form>
   </dialog> */}
