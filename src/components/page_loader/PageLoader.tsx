77ab5b54efa9f249315cd317531fe64b2fcfecac
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-06-30 23:28:30 -0300

    Update PageLoader.tsx

--- src/components/page_loader/PageLoader.tsx
+++ src/components/page_loader/PageLoader.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const PageLoader = () => {
   return (
-    <div>Loading...</div>
+    <div className='flex flex-col justify-center items-center h-[100vh]'>
+      <span className="loading loading-spinner loading-lg text-primary"></span>
+      {/* <div className='flex'><p className='mr-4 text-primary'>Loading</p><span className="loading loading-dots loading-xs text-primary"></span></div> */}
+    </div>
   )
 }
 
