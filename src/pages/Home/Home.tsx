d7d8c29e02f5ed0dff15945075ecf745dac77f9f
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-06-30 23:04:55 -0300

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,14 +4,20 @@ import Header from '../../components/header/Header'
 import Carousel from '../../components/carousel/Carousel'
 import Menu from '../../components/menu/Menu'
 import Footer from '../../components/footer/Footer'
+import OrderDetail from '../OrderDetail/OrderDetail'
+import OrderTracking from '../OrderTracking/OrderTracking'
 
 const Home = () => {
     const myModal = document.getElementById('my_modal_1') as HTMLElement
     return (
         <div>
-            <Carousel />
-            <Menu />
-        </div>
+            {/* <Carousel />
+            <Menu /> */}
+
+            {/* <OrderDetail/> */}
+
+            <OrderTracking />
+        </div >
     )
 }
 
