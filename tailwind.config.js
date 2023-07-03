fa940465ab28b519af3fcd21d72ec5abeb9eed53
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-07-03 11:20:36 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,27 +7,29 @@ export default {
   daisyui: {
     themes: [
       {
-        mytheme: {
-          
+        BS_theme: {
+
           "primary": "#ef4444",
-                   
+
           "secondary": "#fecaca",
-                   
+
           "accent": "#1fb2a6",
-                   
+
           "neutral": "#2a323c",
-                   
+
           "base-100": "#f3f4f6",
-                   
+
           "info": "#3abff8",
-                   
+
           "success": "#36d399",
-                   
+
           "warning": "#facc15",
-                   
+
           "error": "#f87171",
-                   },
+        },
       },
+      "dark",
+      "light",
     ],
   },
   plugins: [require("daisyui")],
