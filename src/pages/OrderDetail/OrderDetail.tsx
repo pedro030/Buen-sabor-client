ce356fc96ccc24be61b25639d4261deba7929e8a
Asiel <asiel.alvarez@alumnos.frm.utn.edu.ar>
2023-07-11 19:59:01 -0300

    Update OrderDetail

--- src/pages/OrderDetail/OrderDetail.tsx
+++ src/pages/OrderDetail/OrderDetail.tsx
@@ -6,6 +6,7 @@ import { CartContext } from '../../context/cart'
 const OrderDetail = () => {
     const { cart }: any = useContext(CartContext);
     const [deliveryTakeAway, setDeliveryTakeAway] = useState(true);
+    const [mp, setMp] = useState(true);
 
     const totalPrice = cart.reduce((total: any, item: any) => {
         const itemPrice = item.price * item.quantity;
@@ -20,7 +21,7 @@ const OrderDetail = () => {
                     <li className="step step-primary">Choice product</li>
                     <li className="step step-primary">Create Order</li>
                     <li className="step step-primary">Follow up</li>
-                    <li className="step">Delivered!</li>
+                    { mp ? <><li className='step'>Pay</li><li className='step'>Delivered!</li></> : <li className="step">Delivered!</li> }
                 </ul>
             </div>
             <div className='flex justify-center'>
@@ -31,8 +32,8 @@ const OrderDetail = () => {
                         <div className="flex justify-center">
                             <div className={deliveryTakeAway ? "grid grid-rows-[50px_1fr] bg-white h-64 w-[80%] rounded-3xl" : "grid grid-rows-[50px_1fr] bg-white h-32 w-[80%] rounded-3xl"}>
                                 <div className="grid grid-cols-2 join">
-                                    <input className="rounded-full join-item btn" type="radio" name="delivery" aria-label="Delivery" onClick={() => setDeliveryTakeAway(true)} />
-                                    <input className="rounded-full join-item btn" type="radio" name="delivery" aria-label="Take Away" onClick={() => setDeliveryTakeAway(false)} />
+                                    <input className="rounded-full join-item btn" type="radio" name="delivery" aria-label="Delivery" onClick={() => {setDeliveryTakeAway(true); setMp(true)}} checked={deliveryTakeAway ? true : false}/>
+                                    <input className="rounded-full join-item btn" type="radio" name="delivery" aria-label="Take Away" onClick={() => setDeliveryTakeAway(false)} checked={!deliveryTakeAway ? true : false}/>
                                 </div>
                                 {deliveryTakeAway ?
                                     <div className="p-4">
@@ -106,8 +107,8 @@ const OrderDetail = () => {
                                 <div>
                                     <h1>Available Methods: </h1>
                                     <div className='flex flex-col items-center justify-between join'>
-                                        <input className={deliveryTakeAway ? "w-full my-4 rounded-none join-item btn btn-disabled" : "w-full my-4 rounded-none join-item btn"} type="radio" name="payment" aria-label="Cash" />
-                                        <input className="w-full rounded-none join-item btn" type="radio" name="payment" aria-label="Mercado Libre" />
+                                        <input className="w-full rounded-none join-item btn" type="radio" name="payment" aria-label="Mercado Pago" checked={mp ? true : false} onClick={() => setMp(true)}/>
+                                        <input className={deliveryTakeAway ? "w-full my-4 rounded-none join-item btn btn-disabled" : "w-full my-4 rounded-none join-item btn"} type="radio" name="payment" aria-label="Cash" onClick={() => setMp(false)}/>
                                     </div>
                                 </div>
                             </div>
@@ -136,7 +137,8 @@ const OrderDetail = () => {
                                     <p className="my-3 text-sm font-bold">${(totalPrice + 100 + 300)}</p>
                                 </div>
                             </div>
-                            <button className="rounded-full btn btn-primary">Make the order</button>
+                            { mp ? <button className="rounded-full btn btn-primary">Go to Pay</button> : <button className="rounded-full btn btn-primary">Make the order</button>}
+                            
                         </div>
                     </div>
                 </div>
