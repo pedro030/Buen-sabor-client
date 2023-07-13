27b0131a682e8f31b0fbf540469b0f208f2bfd07
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-07-13 16:20:00 -0300

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.scss'
 import Header from '../../components/header/Header'
 import Carousel from './carousel/Carousel'
@@ -13,6 +13,16 @@ import { CartProvider } from '../../context/cart'
 import { UserProvider } from '../../context/user'
 
 const Home = () => {
+
+
+    useEffect(() => {
+        console.log('effect')
+        fetch('https://buen-sabor-niqf.onrender.com/api/products/get/1')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }, [])
+
+
     return (
         <div>
                 <Carousel />
