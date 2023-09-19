8184e04c2509a56d915bdae5ca60c79f82eec7c6
Asiel <asiel.alvarez@alumnos.frm.utn.edu.ar>
2023-09-18 22:41:10 -0300

    Logout por Inactividad

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ import { LocationsProvider } from './context/locations';
 
 const  App = () => {
 
-  const { isLoading, user } = useAuth0();
+  const { isLoading, user, logout } = useAuth0();
   const { getUserInfo, tokenUser } = useContext(UserContext);
 
   if (isLoading) {
@@ -39,6 +39,38 @@ const  App = () => {
       if (user.email) getUserInfo(user.email);
     }
   }, [tokenUser])
+
+  // Logout por Inactividad de 45 Minutos
+  useEffect(() => {
+    let inactivityTimeout: NodeJS.Timeout | null = null;
+
+    // Función para reiniciar el temporizador de inactividad
+    const resetInactivityTimeout = () => {
+      if (inactivityTimeout) {
+        clearTimeout(inactivityTimeout);
+      }
+
+      // Configura el tiempo de espera nuevamente
+      inactivityTimeout = setTimeout(() => {
+        logout()
+      }, 45 * 60 * 1000); // 45 minutos en milisegundos
+    };
+
+    // Agrega controladores de eventos para rastrear la actividad
+    window.addEventListener('mousemove', resetInactivityTimeout);
+    window.addEventListener('keydown', resetInactivityTimeout);
+    window.addEventListener('click', resetInactivityTimeout);
+
+    // Inicializa el temporizador de inactividad
+    resetInactivityTimeout();
+
+    return () => {
+      // Limpia los controladores de eventos al desmontar el componente
+      window.removeEventListener('mousemove', resetInactivityTimeout);
+      window.removeEventListener('keydown', resetInactivityTimeout);
+      window.removeEventListener('click', resetInactivityTimeout);
+    };
+  }, []);
   
 
   return (
