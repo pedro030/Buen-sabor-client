77c2caf45b2f904a267e7a6a48ed86afd008c725
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2023-09-28 14:12:41 -0300

    change icons and fixed the modal

--- src/pages/UserProfile/Components/history_order/History_Order.tsx
+++ src/pages/UserProfile/Components/history_order/History_Order.tsx
@@ -5,6 +5,9 @@ import PDFBillModal from './PDFBillModal/PDFBillModal';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import PDFDocument from './PDFDocument/PDFDocument';
 
+import { AiOutlineEye, AiOutlineFilePdf } from 'react-icons/ai'
+
+
 const History_Order = () => {
     const { orders } = useContext(UserContext)
     const [isOpen, setIsOpen] = useState(false);
@@ -17,66 +20,66 @@ const History_Order = () => {
 
     return (
         <>
-        <PDFBillModal
-            obj={bill}
-            isOpen={isOpen}
-            onClose={() => setIsOpen(false)}
-        />
-        <div>
-            <h2 className='mb-5 text-center stat-title'>Order history</h2>
+            <PDFBillModal
+                obj={bill}
+                isOpen={isOpen}
+                onClose={() => setIsOpen(false)}
+            />
+            <div>
+                <h2 className='mb-5 text-center stat-title'>Order history</h2>
 
-            <div className="overflow-x-auto">
-                <table className="table table-xs">
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Withdrawal</th>
-                            <th>Total</th>
-                            <th>Address</th>
-                            <th>Status</th>
-                            <th>Products</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            orders.map((o: any) => {
-                                return <tr key={o.id}>
-                                    <td>{o.date}</td>
-                                    <td>{o.withdrawalMode}</td>
-                                    <td>${o.totalPrice}</td>
-                                    <td>{o.address}</td>
-                                    <td>{o.statusOrder.statusType}</td>
-                                    <td>
-                                        <div tabIndex={0} className='collapse'>
-                                            <div className='flex items-center p-0 collapse-title'>
-                                                List Products
+                <div className="overflow-x-auto">
+                    <table className="table table-xs">
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Withdrawal</th>
+                                <th>Total</th>
+                                <th>Address</th>
+                                <th>Status</th>
+                                <th>Products</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                orders.map((o: any) => (
+                                    <tr key={o.id}>
+                                        <td>{o.creationDate}</td>
+                                        <td>{o.withdrawalMode}</td>
+                                        <td>${o.totalPrice}</td>
+                                        <td>{o.address}</td>
+                                        <td>{o.statusOrder.statusType}</td>
+                                        <td>
+                                            <div tabIndex={0} className='collapse'>
+                                                <div className='flex items-center p-0 collapse-title'>
+                                                    List Products
+                                                </div>
+                                                <div className='p-0 collapse-content'>
+                                                    {o.products.map((p: any) => {
+                                                        return <p>{p.cant}x {p.product.name}</p>
+                                                    })}
+                                                </div>
                                             </div>
-                                            <div className='p-0 collapse-content'>
-                                                {o.products.map((p:any) => {
-                                                    return <p>{p.cant}x {p.product.name}</p>
-                                                })}
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        {<><button onClick={() => viewPDF(o)} className="">View Bill</button>
-                                        <PDFDownloadLink
-                                            document={<PDFDocument obj={o}/>}
-                                            fileName={`Buen Sabor - Order Bill #${o.id}`}
-                                        >
-                                            {({ blob, url, loading, error }) =>
-                                            loading ? "Loading document..." : <button>Download</button>
-                                            }
-                                        </PDFDownloadLink> </>}
-                                    </td>
-                                </tr>
-                            })
-                        }
-                    </tbody>
-                </table>
+                                        </td>
+                                        <td>
+                                            {<><button onClick={() => viewPDF(o)} className=""><AiOutlineEye className='w-6 h-6 ' /></button>
+                                                <PDFDownloadLink
+                                                    document={<PDFDocument obj={o} />}
+                                                    fileName={`Buen Sabor - Order Bill #${o.id}`}
+                                                >
+                                                    {({ blob, url, loading, error }) =>
+                                                        loading ? "Loading document..." : <button><AiOutlineFilePdf className='w-6 h-6 ' /></button>
+                                                    }
+                                                </PDFDownloadLink> </>}
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
         </>
     )
 }
