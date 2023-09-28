90ddd67c0a52c22655bcd5d5155ca4af1c1c05fb
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2023-09-28 12:04:39 -0300

    Update Change_password.tsx

--- src/pages/UserProfile/Components/change_password/Change_password.tsx
+++ src/pages/UserProfile/Components/change_password/Change_password.tsx
@@ -1,12 +1,12 @@
 import * as Yup from 'yup';
-import { ErrorMessage, Field, Form, Formik, FormikHelpers} from 'formik'
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from 'formik'
 import { updatePassword } from '../../../../services/Auth0Service';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useEffect, useState } from 'react';
 
 const Change_password = () => {
-    const {user} = useAuth0();
-    const[canChange, setCanChange] = useState<boolean>(false);
+    const { user } = useAuth0();
+    const [canChange, setCanChange] = useState<boolean>(false);
     const validationSchema = Yup.object({
         // oldPassword: Yup.string().required('Old password is required'),
         newPassword: Yup.string()
@@ -19,23 +19,23 @@ const Change_password = () => {
         confirmPassword: Yup.string()
             .oneOf([Yup.ref('newPassword')], 'Passwords must match')
     })
-    useEffect(()=>{
-        if(user?.sub){
+    useEffect(() => {
+        if (user?.sub) {
             setCanChange(user.sub?.split('|')[0] === "auth0");
         }
-    },[])
+    }, [])
 
-    const handleSubmit = (state: any, action: FormikHelpers<any>) =>{
+    const handleSubmit = (state: any, action: FormikHelpers<any>) => {
         updatePassword(user?.sub || "", state.newPassword)
-        .then(data => {
-            if(data){
-                alert("Password update success")
-                action.resetForm();
-            }else{
-                alert("An error ocurr updating password, please try later");
-                action.resetForm();
-            }
-        })
+            .then(data => {
+                if (data) {
+                    alert("Password update success")
+                    action.resetForm();
+                } else {
+                    alert("An error ocurr updating password, please try later");
+                    action.resetForm();
+                }
+            })
     }
     return (
         <>
@@ -44,8 +44,8 @@ const Change_password = () => {
                 <Formik
                     initialValues={{
                         // oldPassword:"",
-                        newPassword:"",
-                        confirmPassword:""
+                        newPassword: "",
+                        confirmPassword: ""
                     }}
                     onSubmit={handleSubmit}
                     validationSchema={validationSchema}
@@ -73,7 +73,11 @@ const Change_password = () => {
                             <ErrorMessage name="confirmPassword">{msg => <span className="error-message">{msg}</span>}</ErrorMessage>
                         </div>
                         <button disabled={!canChange} className="rounded-full btn btn-primary" type="submit">Save changes</button>
-                { canChange?(<></>):(<span>You can't change the password because you logged with google</span>)}
+                        {canChange ? (<></>) : (
+                            <div className="alert alert-warning">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 stroke-current shrink-0" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+                                <span>Warning: You can't change the password because you logged with google</span>
+                            </div>)}
                     </Form>
                 </Formik>
             </div>
