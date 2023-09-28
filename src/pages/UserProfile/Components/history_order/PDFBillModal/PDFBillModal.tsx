05b01b919b49e4de11b77dd60ef8737a8acc7635
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2023-09-28 14:13:05 -0300

    style pdf

--- src/pages/UserProfile/Components/history_order/PDFBillModal/PDFBillModal.tsx
+++ src/pages/UserProfile/Components/history_order/PDFBillModal/PDFBillModal.tsx
@@ -2,15 +2,16 @@ import { PDFViewer } from '@react-pdf/renderer';
 import { useRef, useEffect } from 'react'
 import PDFDocument from '../PDFDocument/PDFDocument';
 import { MOrder } from '../../../../../models/MOrder';
+import ReactModal from 'react-modal';
 
 
-interface PDFBillModal {
+interface PDFBillModalProps {
     obj?: any;
     isOpen: boolean;
     onClose: () => void;
 }
 
-const PDFBillModal: React.FC<PDFBillModal> = ({ obj, isOpen, onClose }) => {
+const PDFBillModal: React.FC<PDFBillModalProps> = ({ obj, isOpen, onClose }) => {
 
     if (!isOpen) return null;
 
@@ -28,11 +29,16 @@ const PDFBillModal: React.FC<PDFBillModal> = ({ obj, isOpen, onClose }) => {
     }, []);
 
     return (
-        <div ref={modalRef}>
-            <PDFViewer style={{ width: "80%", height: "100vh"}}>
-                <PDFDocument obj={obj}/>
-            </PDFViewer>
-        </div>
+        <ReactModal isOpen={isOpen} onRequestClose={onClose} className="modal-delete">
+            <div className="rounded modal modal-open">
+                <div ref={modalRef} className='w-[80vw] h-[80vh] flex max-lg:flex-col relative rounded-3xl bg-base-100 z-10 mt-[6rem] overflow-auto'>
+
+                    <PDFViewer style={{ width: "100%", height: "100%" }}>
+                        <PDFDocument obj={obj} />
+                    </PDFViewer>
+                </div>
+            </div>
+        </ReactModal>
     )
 }
 
--- src/pages/UserProfile/Components/history_order/PDFDocument/PDFDocument.tsx
+++ src/pages/UserProfile/Components/history_order/PDFDocument/PDFDocument.tsx
@@ -6,66 +6,67 @@ import { UserContext } from '../../../../../context/user';
 function PDFDocument({ obj }: any) {
     const { userInfo } = useContext(UserContext);
 
-  return (
-    <Document title={`Order #${obj.id}`}>
-                <Page size="A4">
+    return (
+        <Document title={`Order #${obj.id}`}>
+            <Page size="A4">
+                <View>
+                    {/*HEADER*/}
                     <View>
-                        {/*HEADER*/}
                         <View>
-                            <View>
-                                <Image src={"#logoBuenSabor"}/>
-                            </View>
-                            <View>
-                                <Text>Order Bill #{obj.id}</Text>
-                            </View>
+                            {/* className=' font-bold text-red-600 min-w-[28px] ml-10 max-lg:mx-1' */}
+                            <Text>Buen Sabor</Text>
                         </View>
-                        {/*DATA CLIENT & RESTAURANT*/}
                         <View>
-                            <View>
-                                <Text>Client: {userInfo.firstName} {userInfo.lastName}</Text>
-                                <Text>Address: {obj.address}</Text>
-                                <Text>Mail: {userInfo.mail}</Text>
-                                <Text>Cel: {''}</Text>
-                                <Text>Date: {obj.date}</Text>
-                                <Text>Due Date: {obj.paymode.paymode == 'MercadoPago' ? 'ONLINE PAYMENTH' : ''}</Text>
-                            </View>
-                            <View>
-                                <Text>Restaurant: Buen Sabor</Text>
-                                <Text>Addres: Coronel Rodriguez</Text>
-                                <Text>Mail: mailEmpresa@gmail.com</Text>
-                                <Text>Cel: 123456789</Text>
-                            </View>
-                        </View>
-                        {/*TABLE*/}
-                        <View>
-                            {/*TABLE HEADER*/}
-                            <View>
-                                <View><Text>PRODUCT</Text></View>
-                                <View><Text>QTY</Text></View>
-                                <View><Text>UNIT PRICE</Text></View>
-                                <View><Text>SUBTOTAL</Text></View>
-                            </View>
-                            {/*TABLE BODY*/}
-                            {obj.products.map((p:any) => {
-                                return <View>
-                                    <View><Text>{p.product.name}</Text></View>
-                                    <View><Text>{p.cant}</Text></View>
-                                    <View><Text>{p.product.price}</Text></View>
-                                    <View><Text>{p.product.price*p.cant}</Text></View>
-                                </View>
-                            })}
-                            {/*TOTAL*/}
-                            <View>
-                                <View><Text>PRODUCTS COST:</Text><Text>${obj.withdrawalMode == 'Delivery' ? obj.totalPrice - 400 : obj.totalPrice - 100}</Text></View>
-                                <View><Text>SERVICE FEE.:</Text><Text>$100</Text></View>
-                                {obj.withdrawalMode == 'Delivery' ? <View><Text>SHIPPING COST:</Text><Text>$300</Text></View> : ''}
-                                <View><Text>TOTAL:</Text><Text>{obj.totalPrice}</Text></View>
-                            </View>
+                            <Text>Order Bill #{obj.id}</Text>
                         </View>
                     </View>
-                </Page>
-            </Document>
-  )
+                    {/*DATA CLIENT & RESTAURANT*/}
+                    <View>
+                        <View>
+                            <Text>Client: {userInfo.firstName} {userInfo.lastName}</Text>
+                            <Text>Address: {obj.address}</Text>
+                            <Text>Mail: {userInfo.mail}</Text>
+                            <Text>Cel: {''}</Text>
+                            <Text>Date: {obj.date}</Text>
+                            <Text>Due Date: {obj.paymode.paymode == 'MercadoPago' ? 'ONLINE PAYMENTH' : ''}</Text>
+                        </View>
+                        <View>
+                            <Text>Restaurant: Buen Sabor</Text>
+                            <Text>Addres: Coronel Rodriguez</Text>
+                            <Text>Mail: mailEmpresa@gmail.com</Text>
+                            <Text>Cel: 123456789</Text>
+                        </View>
+                    </View>
+                    {/*TABLE*/}
+                    <View>
+                        {/*TABLE HEADER*/}
+                        <View>
+                            <View><Text>PRODUCT</Text></View>
+                            <View><Text>QTY</Text></View>
+                            <View><Text>UNIT PRICE</Text></View>
+                            <View><Text>SUBTOTAL</Text></View>
+                        </View>
+                        {/*TABLE BODY*/}
+                        {obj.products.map((p: any, index: number) => {
+                            return <View key={index}>
+                                <View><Text>{p.product.name}</Text></View>
+                                <View><Text>{p.cant}</Text></View>
+                                <View><Text>{p.product.price}</Text></View>
+                                <View><Text>{p.product.price * p.cant}</Text></View>
+                            </View>
+                        })}
+                        {/*TOTAL*/}
+                        <View>
+                            <View><Text>PRODUCTS COST:</Text><Text>${obj.withdrawalMode == 'Delivery' ? obj.totalPrice - 400 : obj.totalPrice - 100}</Text></View>
+                            <View><Text>SERVICE FEE.:</Text><Text>$100</Text></View>
+                            {obj.withdrawalMode == 'Delivery' ? <View><Text>SHIPPING COST:</Text><Text>$300</Text></View> : ''}
+                            <View><Text>TOTAL:</Text><Text>{obj.totalPrice}</Text></View>
+                        </View>
+                    </View>
+                </View>
+            </Page>
+        </Document>
+    )
 }
 
 export default PDFDocument
