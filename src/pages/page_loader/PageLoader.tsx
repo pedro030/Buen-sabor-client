77fc97e6b2731328323dabac115c212db6c7a5a6
Asiel <asiel.alvarez@alumnos.frm.utn.edu.ar>
2023-10-14 16:48:50 -0300

    PageLoader Cleaning

--- src/pages/page_loader/PageLoader.tsx
+++ src/pages/page_loader/PageLoader.tsx
@@ -1,10 +1,7 @@
-import React from 'react'
-
 const PageLoader = () => {
   return (
     <div className='flex flex-col justify-center items-center h-[100vh]'>
       <span className="loading loading-spinner loading-lg text-primary"></span>
-      {/* <div className='flex'><p className='mr-4 text-primary'>Loading</p><span className="loading loading-dots loading-xs text-primary"></span></div> */}
     </div>
   )
 }
